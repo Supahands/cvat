2dc8152d5a14afdbf580421f23927f08b3d90db6
mohie93 <mohie_93@hotmail.com>
2020-10-12 17:00:48 +0800

    configured local docker environement

---
+++ cvat/settings/local.py
@@ -0,0 +1,54 @@
+# Copyright (C) 2018 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from .base import *
+
+DEBUG = False
+
+INSTALLED_APPS += [
+    'mod_wsgi.server',
+]
+
+NUCLIO['HOST'] = os.getenv('CVAT_NUCLIO_HOST', 'nuclio')
+
+for key in RQ_QUEUES:
+    RQ_QUEUES[key]['HOST'] = os.getenv('CVAT_REDIS_HOST', 'cvat_redis')
+
+CACHEOPS_REDIS['host'] = os.getenv('CVAT_REDIS_HOST', 'cvat_redis')
+
+# Django-sendfile:
+# https://github.com/johnsensible/django-sendfile
+SENDFILE_BACKEND = 'sendfile.backends.xsendfile'
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('CVAT_POSTGRES_HOST', 'cvat_db'),
+        'NAME': os.getenv('CVAT_POSTGRES_DBNAME', 'cvat'),
+        'USER': os.getenv('CVAT_POSTGRES_USER', 'root'),
+        'PASSWORD': os.getenv('CVAT_POSTGRES_PASSWORD', ''),
+    }
+}
+
+# dev only, need production ready settings
+CORS_ALLOWED_ORIGINS = (
+    'http://localhost:3000',
+    'http://supa-cvat-ui.s3-ap-southeast-1.amazonaws.com',
+    'https://supa-ui-exp.supahands.com',
+)
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS= True
