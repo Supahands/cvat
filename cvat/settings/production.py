3c0f18ae541443d91c194ab4e2f5689e914fde6c
mohie93 <mohie_93@hotmail.com>
2020-10-20 16:24:45 +0800

    try 1 adding more cors configs

--- cvat/settings/production.py
+++ cvat/settings/production.py
@@ -41,6 +41,30 @@ CORS_ALLOWED_ORIGINS = (
     'https://supa-ui-exp.supahands.com',
 )
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://supa-cvat-ui.s3-ap-southeast-1.amazonaws.com',
+    'https://supa-ui-exp.supahands.com',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'http://supa-cvat-ui.s3-ap-southeast-1.amazonaws.com',
+    'https://supa-ui-exp.supahands.com',
+]
+
 CORS_ALLOW_METHODS = [
     'DELETE',
     'GET',
