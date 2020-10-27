51716b04c0935eb1cd06c317e891b62b18871335
mohie93 <mohie_93@hotmail.com>
2020-10-27 10:42:09 +0800

    changed nginx configs, production.py configs

--- cvat/settings/production.py
+++ cvat/settings/production.py
@@ -34,24 +34,24 @@ DATABASES = {
     }
 }
 
-# dev only, need production ready settings
-CORS_ALLOWED_ORIGINS = (
-    'http://localhost:3000',
-    'http://supa-cvat-ui.s3-ap-southeast-1.amazonaws.com',
-    'https://supa-ui-exp.supahands.com',
-)
+# # dev only, need production ready settings
+# CORS_ALLOWED_ORIGINS = (
+#     'http://localhost:3000',
+#     'http://supa-cvat-ui.s3-ap-southeast-1.amazonaws.com',
+#     'https://supa-ui-exp.supahands.com',
+# )
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://supa-cvat-ui.s3-ap-southeast-1.amazonaws.com',
-    'https://supa-ui-exp.supahands.com',
-]
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000',
+#     'http://supa-cvat-ui.s3-ap-southeast-1.amazonaws.com',
+#     'https://supa-ui-exp.supahands.com',
+# ]
 
-CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:3000',
-    'http://supa-cvat-ui.s3-ap-southeast-1.amazonaws.com',
-    'https://supa-ui-exp.supahands.com',
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     'http://localhost:3000',
+#     'http://supa-cvat-ui.s3-ap-southeast-1.amazonaws.com',
+#     'https://supa-ui-exp.supahands.com',
+# ]
 
 CORS_ALLOW_METHODS = [
     'DELETE',
